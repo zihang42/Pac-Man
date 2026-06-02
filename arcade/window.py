b12b5bbbbec82a95b4f16e2a6ee2c573ee636efc
Lohann-cv <lohanncoantverdier@gmail.com>
2026-06-02 15:16:21 +0200

    add: Arcade architecture

---
+++ arcade/window.py
@@ -0,0 +1,21 @@
+'''
+    The main window of arcade where
+    everything will be display
+'''
+import arcade
+from views import MainMenu, GameOver, ScoreBoard, Victory
+
+class Pacman():
+    def __init__():
+        self.window = arcade.Window(
+            width: int = 1280,
+            height: int = 720,
+            title: str = 'Arcade Window',
+            fullscreen: bool = False,
+            center_window: bool = True,
+            enable_polling: bool = True,
+        )
+        self.menu = 
+        self.
+
+
