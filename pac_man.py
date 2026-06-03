ef2fe2d5d8a15c65e9c4c6383200f66f951bff93
zihang42 <zihang.wang@learner.42.tech>
2026-06-03 13:46:57 +0200

    fix: style

--- pac_man.py
+++ pac_man.py
@@ -1,6 +1,5 @@
-from src.maze import MazeLoader, Maze
+from src.maze import Maze, MazeLoader
 from src.parser import Parser
-
 from visualizer import Window
 
 
@@ -10,9 +9,7 @@ def main() -> None:
     config = parser.load()
     maze = MazeLoader(config.levels[0]).load()
     print_maze(maze)
-    pacman_visu = Window(config.window_width,
-                         config.window_height,
-                         config.fps)
+    pacman_visu = Window(config.window_width, config.window_height, config.fps)
     pacman_visu.start()
 
 
